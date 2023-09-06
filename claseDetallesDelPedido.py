a8ff6c32f05af32621ac5049564efa6d5b3cecfa
Azul144 <134295386+Azul144@users.noreply.github.com>
2023-09-06 19:48:33 -0300

    Update claseDetallesDelPedido.py

--- claseDetallesDelPedido.py
+++ claseDetallesDelPedido.py
@@ -10,29 +10,41 @@ class DetallesDelPedido():
         # Calcula el subtotal del pedido llamando al método calcPrice()
         self.__subtotal = self.calcPrice()
 
-    def get(self, name):
-        if(name=="orderId"):
-            return self.__orderId
-        elif(name=="productId"):
-            return self.__productId
-        elif(name=="quantity"):
-            return self.__quantity
-        elif(name=="unitCost"):
-            return self.__unitCost
-        elif(name=="subtotal"):
-            return self.__subtotal
+  # Método para obtener el valor de una variable de instancia
+def get(self, name):
+    # Si el nombre es "orderId", devuelve el valor de la variable __orderId
+    if(name=="orderId"):
+        return self.__orderId
+    # Si el nombre es "productId", devuelve el valor de la variable __productId
+    elif(name=="productId"):
+        return self.__productId
+    # Si el nombre es "quantity", devuelve el valor de la variable __quantity
+    elif(name=="quantity"):
+        return self.__quantity
+    # Si el nombre es "unitCost", devuelve el valor de la variable __unitCost
+    elif(name=="unitCost"):
+        return self.__unitCost
+    # Si el nombre es "subtotal", devuelve el valor de la variable __subtotal
+    elif(name=="subtotal"):
+        return self.__subtotal
 
-    def set(self, name, valor):
-        if(name=="orderId"):
-            self.__orderId = valor
-        elif(name=="productId"):
-            self.__productId = valor
-        elif(name=="quantity"):
-            self.__quantity = valor
-            self.__subtotal = self.calcPrice()
-        elif(name=="unitCost"):
-            self.__unitCost = valor
-            self.__subtotal = self.calcPrice()
+# Método para establecer el valor de una variable de instancia
+def set(self, name, valor):
+    # Si el nombre es "orderId", establece el valor de la variable __orderId
+    if(name=="orderId"):
+        self.__orderId = valor
+    # Si el nombre es "productId", establece el valor de la variable __productId
+    elif(name=="productId"):
+        self.__productId = valor
+    # Si el nombre es "quantity", establece el valor de la variable __quantity y recalcula el subtotal
+    elif(name=="quantity"):
+        self.__quantity = valor
+        self.__subtotal = self.calcPrice()
+    # Si el nombre es "unitCost", establece el valor de la variable __unitCost y recalcula el subtotal
+    elif(name=="unitCost"):
+        self.__unitCost = valor
+        self.__subtotal = self.calcPrice()
+
     
     # Método para calcular el precio del pedido
     def calcPrice(self):
