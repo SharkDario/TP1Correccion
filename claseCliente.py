b1ac0e3f76196114b7ed24724d10bbb1567d5dd0
Ald04 <82738067+Ald04@users.noreply.github.com>
2023-09-06 19:20:10 -0300

    Update claseCliente.py

--- claseCliente.py
+++ claseCliente.py
@@ -1,6 +1,9 @@
 from claseUsuario import Usuario
-
+#define una clase llamada Cliente que hereda de la clase Usuario. 
 class Cliente(Usuario):
+#El constructor de la clase Cliente se utiliza para inicializar los atributos de un objeto de tipo Cliente.
+# se pasan los argumentos userId, password, loginStatus y registerDate utilizando super() para inicializar los atributos de autenticación heredados de la clase base.
+#se inicializan varios atributos específicos de la clase Cliente, como customerName, address, email, creditCardInfo, shippingInfo y accountBalance.
     def __init__(self, userId, password, loginStatus, registerDate, customerName, address, email, creditCardInfo, accountBalance, shippingInfo=[]):
         super().__init__(userId, password, loginStatus, registerDate)
         self.__customerName = customerName
@@ -11,6 +14,9 @@ class Cliente(Usuario):
         self.__shippingInfo = shippingInfo 
         self.__accountBalance = accountBalance
 
+
+    #el metodo get permite obtener valores de atributos tanto heredados de la clase base Usuario como específicos de la clase Cliente.
+    #Se utilizan super() para acceder al metodo get de la clase base cuando se trata de atributos heredados.
     def get(self, name):
         if(name=="userId"):
             return super().get("userId")
@@ -32,7 +38,9 @@ class Cliente(Usuario):
             return self.__shippingInfo
         elif(name=="accountBalance"):
             return self.__accountBalance
-
+            
+#El metodo set permite modificar atributos de la instancia
+#Se utilizan super() para acceder al metodo set de la clase base cuando se trata de atributos heredados.
     def set(self, name, valor):
         if(name=="userId"):
             super().set("userId", valor)
@@ -55,6 +63,10 @@ class Cliente(Usuario):
         elif(name=="accountBalance"):
             self.__accountBalance = valor
 
+#El método register acepta dos argumentos: email y password. Estos argumentos representan la dirección de correo electrónico y la contraseña que el cliente desea asociar con su cuenta.
+#Dentro del método, se asigna el valor de email al atributo privado __email de la instancia actual de Cliente. Esto actualiza la dirección de correo electrónico del cliente en la instancia.
+#se asigna el valor de password al atributo privado password de la instancia actual de Cliente. Esto actualiza la contraseña del cliente en la instancia.
+    
     def register(self, email, password):
         self.__email = email
         self.__password = password
