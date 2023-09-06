c4ecee3f2e77d422579d45a4043469364aa3fa1b
Ald04 <82738067+Ald04@users.noreply.github.com>
2023-09-06 19:44:58 -0300

    Update clasePedido.py

--- clasePedido.py
+++ clasePedido.py
@@ -3,6 +3,9 @@ from claseDetallesDelPedido import DetallesDelPedido
 from datetime import datetime
 
 class Pedido():
+    #El constructor __init__ se utiliza para inicializar un objeto de la clase Pedido cuando se crea una instancia de ella.
+    #Dentro del constructor, se formatea la fecha de creación (dateCreated) y la fecha de envío (dateShipped) utilizando el módulo datetime para asegurarse de que estén en el formato 'dd/mm/yyyy'.
+    #se crea una instancia de las clases InformacionDelPedido y DetallesDelPedido utilizando ciertos atributos del pedido para almacenar información relacionada con el envío y los detalles del pedido.
     def __init__(self, orderId, dateCreated, dateShipped, customerName, customerId, status, shippingId, shippingType, shippingCost, shippingRegionId, productId, productName, quantity, unitCost):
         self.__orderId = orderId
         self.__dateCreated = datetime.strftime(dateCreated, '%d/%m/%Y')
@@ -15,6 +18,9 @@ class Pedido():
         self.__informacionDelPedido = InformacionDelPedido(shippingId, shippingType, shippingCost, shippingRegionId)
         self.__detallesDelPedido = DetallesDelPedido(orderId, productId, productName, quantity, unitCost)
 
+#Este metodo se utiliza para realizar un pedido en el sistema.
+#Toma varios argumentos relacionados con el pedido, como el nombre del cliente, el ID del cliente, el ID del producto, el nombre del producto, la cantidad, el costo unitario, el tipo de envío, el costo de envío y la región de envío.
+#Dentro del método, se actualiza la información del pedido (informacionDelPedido) y los detalles del pedido (detallesDelPedido) con la nueva información proporcionada.
     def placeOrder(self, customerName, customerId, productId, productName, quantity, unitCost, shippingType, shippingCost, shippingRegionId):
         self.__informacionDelPedido = InformacionDelPedido(self.__shippingId, shippingType, shippingCost, shippingRegionId)
         self.__detallesDelPedido = DetallesDelPedido(self.__orderId, productId, productName, quantity, unitCost)
