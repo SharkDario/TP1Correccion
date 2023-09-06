9d3f931606d717367e5acb63add325cf65ab3172
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-09-06 19:46:33 -0300

    Update claseDetallesDelPedido.py

--- claseDetallesDelPedido.py
+++ claseDetallesDelPedido.py
@@ -10,6 +10,30 @@ class DetallesDelPedido():
         # Calcula el subtotal del pedido llamando al método calcPrice()
         self.__subtotal = self.calcPrice()
 
+    def get(self, name):
+        if(name=="orderId"):
+            return self.__orderId
+        elif(name=="productId"):
+            return self.__productId
+        elif(name=="quantity"):
+            return self.__quantity
+        elif(name=="unitCost"):
+            return self.__unitCost
+        elif(name=="subtotal"):
+            return self.__subtotal
+
+    def set(self, name, valor):
+        if(name=="orderId"):
+            self.__orderId = valor
+        elif(name=="productId"):
+            self.__productId = valor
+        elif(name=="quantity"):
+            self.__quantity = valor
+            self.__subtotal = self.calcPrice()
+        elif(name=="unitCost"):
+            self.__unitCost = valor
+            self.__subtotal = self.calcPrice()
+    
     # Método para calcular el precio del pedido
     def calcPrice(self):
         # Calcula el precio multiplicando la cantidad por el costo unitario
